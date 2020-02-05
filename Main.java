48f599b4cc8434f4b5de7d203fdf7cec2a511d3a
Erick <elago@nelsonserver.local>
2020-02-05 10:03:47 +0100

    Version2

--- Main.java
+++ Main.java
@@ -1,23 +1,25 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<String>datos = new ArrayList<String>();
 		Scanner sc = new Scanner (System.in);
 		Scanner sn = new Scanner (System.in);
 		System.out.println("Introduce tu nombre");
-		String nombre = sc.nextLine();
+		datos.add(sc.nextLine());
 		System.out.println("Introduce tu apellido");
-		String apellido = sc.nextLine();
+		datos.add(sc.nextLine());
 		System.out.println("Introduce tu fecha de nacimiento");
-		String fnacimiento = sc.nextLine();
+		datos.add(sc.nextLine());
 		System.out.println("Introduce tu dni");
-		String dni = sc.nextLine();
+		datos.add(sc.nextLine());
 		System.out.println("Introduce tus estudios previos");
-		String estudios = sc.nextLine();
+		datos.add(sc.nextLine());
 		System.out.println("Introduce tu telefono");
-		String tlf = sc.nextLine();
+		datos.add(sc.nextLine());
 		
 	}
 
